3f36c5bb91806c10572d9e7b737cd2716155775a
mehty <mmahdi.nasiri@yahoo.com>
2026-04-17 20:13:20 +0330

    feat: graceful shutdown

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"context"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	"sample-web-http/internal/handler"
 	redisclient "sample-web-http/internal/redis"
 	"sample-web-http/internal/route"
 	"sample-web-http/internal/todo"
+	"syscall"
 	"time"
 )
 
@@ -25,6 +30,22 @@ func main() {
 		WriteTimeout: 10 * time.Second,
 		IdleTimeout:  120 * time.Second,
 	}
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Println("Error server:", err)
+		}
+	}()
 
-	server.ListenAndServe()
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	<-ctx.Done()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	server.Shutdown(ctx)
+	log.Println("Server shutdown complete")
+	rdb.Close()
+	log.Println("Redis connection closed")
 }
